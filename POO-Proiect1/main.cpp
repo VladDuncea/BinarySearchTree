f8f5aeb83c303dd231de38302d439f2c4157509f
Duncea Vlad <vlad_duncea_31@yahoo.com>
2019-03-02 21:41:33 +0200

    New methods to binary search tree

--- POO-Proiect1/main.cpp
+++ POO-Proiect1/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class nod
 {
 	int data;
-	nod * stanga,* dreapta;
+	nod * stanga,* dreapta,*father;
 	friend class arborebinarcautare;
 
 public:
@@ -33,14 +33,18 @@ class arborebinarcautare
 
 	}
 
-	void priv_afisare(nod *n,ostream &os)
+	void priv_afisareSRD(nod *n,ostream &os)
 	{
 		if (n == NULL)
 			return;
-		priv_afisare(n->stanga,os);
+		priv_afisareSRD(n->stanga,os);
 		os << n->data<<" ";
-		priv_afisare(n->dreapta,os);
+		priv_afisareSRD(n->dreapta,os);
 	}
+
+	void priv_afisare_frunze(nod *n);
+	int priv_adancime(nod *n,int level);
+	void priv_stergere(nod *n,int x);
 
 public:
 
@@ -87,23 +91,27 @@ public:
 		return;
 	}
 
-	void afisare(ostream &os)
+	void afisareSRD(ostream &os)
 	{
-		priv_afisare(root,os);
+		priv_afisareSRD(root,os);
 	}
 
-	void afisare()
+	void afisareSRD()
 	{
-		priv_afisare(root, cout);
+		priv_afisareSRD(root, cout);
 	}
 
-	void  
+	void stergere(int x);
+
+	void afisare_frunze(void);
+
+	int adancime(void);
 };
 
 
 ostream & operator<< (ostream &os,arborebinarcautare a)
 {
-	a.afisare();
+	a.afisareSRD(os);
 	return os;
 }
 
@@ -116,8 +124,93 @@ int main()
 	a.adaugare(7);
 	a.adaugare(10);
 	a.adaugare(8);
-	a.afisare();
+	a.afisareSRD();
+	cout << endl;
+	a.afisare_frunze();
+	cout << endl;
+	cout << a.adancime();
 
-	cout << a<<a;
 	return 0;
+}
+
+void arborebinarcautare::priv_afisare_frunze(nod * n)
+{
+	if (n == NULL)
+		return;
+
+	if (n->stanga == NULL && n->dreapta == NULL)
+		cout << n->data<<" ";
+	else
+	{
+		priv_afisare_frunze(n->stanga);
+		priv_afisare_frunze(n->dreapta);
+	}
+}
+
+int arborebinarcautare::priv_adancime(nod * n,int level)
+{
+	int s,d;
+	s = d = level;
+	if (n->dreapta != NULL)
+		d = priv_adancime(n->dreapta, level + 1);
+	if (n->stanga != NULL)
+		s = priv_adancime(n->stanga, level + 1);
+	return s < d ? d : s;
+}
+
+void arborebinarcautare::priv_stergere(nod * n,int x)
+{
+	if (n == NULL)
+		return;
+
+	if (n->data == x)
+	{
+		if (n->stanga == NULL || n->dreapta == NULL)
+		{
+			if (n->stanga == NULL && n->dreapta == NULL)
+			{
+				if (n->father->data < n->data)
+					n->father->dreapta = NULL;
+				else
+					n->father->stanga = NULL;
+				delete n;
+				return;
+			}
+
+			if (n->data < n->father->data)
+				n->father->stanga = (n->stanga == NULL) ? n->dreapta : n->stanga;
+			else
+				n->father->dreapta = (n->stanga == NULL) ? n->dreapta : n->stanga;
+
+			delete n;
+			return;
+		}
+
+		//The node has both children
+		nod *aux=n->dreapta;
+		while (aux->stanga != NULL)
+			aux = aux->stanga;
+
+	}
+	
+	if (n->data < x)
+		priv_stergere(n->dreapta, x);
+	else
+		priv_stergere(n->stanga, x);
+		
+}
+
+void arborebinarcautare::stergere(int x)
+{
+	priv_stergere(root,x);
+}
+
+void arborebinarcautare::afisare_frunze(void)
+{
+	priv_afisare_frunze(root);
+}
+
+int arborebinarcautare::adancime(void)
+{
+	return priv_adancime(root,1);
 }
